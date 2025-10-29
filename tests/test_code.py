41571d8430219e43bcf5903487b6cd68cbde1eba
R1jul <rijul01b@gmail.com>
2025-10-29 18:58:20 +0530

    The deploying of the Flask app

---
+++ tests/test_code.py
@@ -0,0 +1,11 @@
+from code2 import code2
+
+def test_kg_lb():
+    client=code2.test_client()
+    resp=client.get("/convert? value=1 & unit=kg")
+    assert b"2.20 lb" in resp.data
+    
+def test_lb_kg():
+    client=code2.test_client()
+    resp=client.get("/convert? value=2.20 & unit=lb")
+    assert b"1.00 kg" in resp.data
